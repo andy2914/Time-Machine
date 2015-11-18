0287c615d5c2dbd40e7f66f0f6525c502f60b279
andy2914 <andrew.abraham87@gmail.com>
2015-11-18 13:05:24 -0500

    Update SetTimeMachineDestination.sh

--- SetTimeMachineDestination.sh
+++ SetTimeMachineDestination.sh
@@ -2,10 +2,6 @@
 
 ##Setting the Time Machine destination here
 
-##tmutil REQUIRES a username and password be set in the URL as we're doing below. Recommendation is to
-##create a new account on your server that has read/write access over the TM volume and set that username/password
-##combo in the URL below, replacing tmuser:tmpassword.
-
 TMDestination="afp://tmuser:tmpassword@tmserver.org/Time_Machine_Backups"
 
 echo "Setting Time Machine Destination"
